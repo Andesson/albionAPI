6f6f2cacfe810a6300dd1c88e234b492518a310a
Andesson <andessonlins93@gmail.com>
2023-08-08 01:59:02 -0300

    pequeno ajuste no Apiservice, retirado codigo comentado

--- src/main/java/com/anubis/albion/services/ApiService.java
+++ src/main/java/com/anubis/albion/services/ApiService.java
@@ -10,21 +10,6 @@ public class ApiService {
 
     private final WebClient client = WebClient.create();
 
-    /*
-    public Mono<String> findByName(String url) {
-        return client.get()
-                .uri(url)
-                .retrieve()
-                .bodyToMono(String.class)
-                //chamada Async
-                .flatMap(body -> Mono.fromCallable(() -> {
-                    // Realize operações potencialmente bloqueantes aqui
-                    return body;
-                }).subscribeOn(Schedulers.boundedElastic()));
-                //chamada Sync
-                //.block();
-    }
-    */
     public Mono<String> findByName(String name) {
         String url = System.getProperty("ALBION_URL") + System.getProperty("ALBION_SEARCH") + name;
         return client.get()
