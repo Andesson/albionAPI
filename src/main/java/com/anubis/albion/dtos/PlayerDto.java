af58ab8ec75f07e6c92fa59c5a2d656290878e52
Andesson <andessonlins93@gmail.com>
2023-08-08 03:10:00 -0300

    Manipulando o retorno JSON da API para o objeto

--- src/main/java/com/anubis/albion/dtos/PlayerDto.java
+++ src/main/java/com/anubis/albion/dtos/PlayerDto.java
@@ -3,15 +3,13 @@ package com.anubis.albion.dtos;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
-import java.util.UUID;
-
 @Data
 public class PlayerDto {
 
     @NotBlank
-    private UUID player_id;
-    @NotBlank
     private String player_name;
     @NotBlank
     private String player_code;
+    @NotBlank
+    private String kda;
 }
